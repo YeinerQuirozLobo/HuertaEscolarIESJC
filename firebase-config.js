cb2d07308ad367da8e780716f998c0d7e316da78
YeinerQuirozLobo <Yeinerquirozlobo93@gmail.com>
2025-08-31 15:29:08 -0500

    xcd

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDjK8gm7yhBbX3dv6wwV5fFfoS8CeaZ6u0",
+  authDomain: "huerta-escolar-iesjc.firebaseapp.com",
+  projectId: "huerta-escolar-iesjc",
+  storageBucket: "huerta-escolar-iesjc.firebasestorage.app",
+  messagingSenderId: "76068797718",
+  appId: "1:76068797718:web:5f3e954a06014e5c57a437"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
