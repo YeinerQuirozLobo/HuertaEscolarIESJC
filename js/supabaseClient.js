7d1f58f36610c68d134e271158140278f50ce349
YeinerQuirozLobo <Yeinerquirozlobo93@gmail.com>
2025-09-23 20:16:38 -0500

    l{

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,14 +1,14 @@
 // js/supabaseClient.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+// ⚡ Copia y pega tu URL y Anon Key desde Supabase > Project Settings > API
 const SUPABASE_URL = "https://mhjfofucyvlesahsekzv.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oamZvZnVjeXZsZXNhaHNla3p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1NzM2NjMsImV4cCI6MjA3NDE0OTY2M30.wDV6wH5pfqSzs6FSkArucqIWS2Q5_PvyczNLQ0_mJZk";
 
-// Use this configuration to ensure the session is persistent
+// Configuración del cliente de Supabase para persistir la sesión
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    persistSession: true, // This is crucial for keeping the session alive
-    autoRefreshToken: true,
+    persistSession: true, // Esto es crucial para mantener la sesión en el navegador
   },
 });
 
